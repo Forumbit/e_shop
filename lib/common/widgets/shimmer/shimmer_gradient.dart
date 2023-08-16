2161bbc62c452177add5121e5efd1d68ba044eb6
Amir Kharisov <58177045+Forumbit@users.noreply.github.com>
2023-08-16 16:26:07 +0300

    added: shimmer

---
+++ lib/common/widgets/shimmer/shimmer_gradient.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+abstract class ShimmerGradient {
+  static const shimmerGradient = LinearGradient(
+    colors: [
+      Color(0xFFEBEBF4),
+      Color(0xFFF4F4F4),
+      Color(0xFFEBEBF4),
+    ],
+    stops: [
+      0.1,
+      0.3,
+      0.4,
+    ],
+    begin: Alignment(-1.0, -0.3),
+    end: Alignment(1.0, 0.3),
+    tileMode: TileMode.clamp,
+  );
+}
