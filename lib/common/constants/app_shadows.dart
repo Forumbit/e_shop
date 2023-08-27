e0f69e142723dd1cc96b0aaf9f379868a6835f4f
Amir Kharisov <58177045+Forumbit@users.noreply.github.com>
2023-08-28 01:13:49 +0300

    added: common gradients, shadows

---
+++ lib/common/constants/app_shadows.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+abstract class AppShadows {
+  static const itemShadow = <BoxShadow>[
+    BoxShadow(
+      color: Colors.black12,
+      blurRadius: 8,
+    ),
+  ];
+}
