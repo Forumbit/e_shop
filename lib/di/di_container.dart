9c269dba28a1554aa8d2b57ec21a7d3ee4e8c59b
Amir Kharisov <58177045+Forumbit@users.noreply.github.com>
2023-09-03 11:18:20 +0300

    di container: added cart feature

--- lib/di/di_container.dart
+++ lib/di/di_container.dart
@@ -1,8 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dio/dio.dart';
 import 'package:e_shop/factories/screen_factory.dart';
 import 'package:e_shop/features/auth/data/datasources/auth_remote_datasource.dart';
 import 'package:e_shop/features/auth/data/repository/auth_repository.dart';
 import 'package:e_shop/features/auth/domain/repository/auth_repository.dart';
+import 'package:e_shop/features/cart/data/datasources/cart_product_remote_data_source.dart';
+import 'package:e_shop/features/cart/data/datasources/cart_remote_data_source.dart';
+import 'package:e_shop/features/cart/data/repository/cart_product_repository.dart';
+import 'package:e_shop/features/cart/data/repository/cart_repository.dart';
+import 'package:e_shop/features/cart/domain/repository/cart_product_repository.dart';
+import 'package:e_shop/features/cart/domain/repository/cart_repository.dart';
 
 import 'package:e_shop/features/category/data/datasources/category_remote_datasource.dart';
 import 'package:e_shop/features/category/data/repository/category_repository.dart';
@@ -19,17 +26,34 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 class DIContainer {
   final _dio = Dio();
-
   //* ============= app feature =============
   ScreenFactory _makeScreenFactory() => ScreenFactoryDefault();
   AppRoute makeRoute() => AppGoRoute(screenFactory: _makeScreenFactory());
 
   //* ============= auth feature =============
-  AuthRemoteDataSource _getRemoteDataSource() =>
+  AuthRemoteDataSource _getAuthRemoteDataSource() =>
       AuthRemoteDataSourceImpl(FirebaseAuth.instance);
 
   AuthRepository getAuthRepository() =>
-      AuthRepositoryImpl(_getRemoteDataSource());
+      AuthRepositoryImpl(_getAuthRemoteDataSource());
+
+  //* ============= cart feature ===============
+  CartRemoteDataSource _getCartRemoteDataSource() => CartRemoteDataSourceImpl(
+        firebaseFirestore: FirebaseFirestore.instance,
+      );
+  CartProductRemoteDataSource _getCartProductRemoteDataSource() =>
+      CartProductRemoteDataSourceImpl(
+        FirebaseFirestore.instance,
+      );
+
+  CartRepository getCartRepository() => CartRepositoryImpl(
+      cartRemoteDataSource: _getCartRemoteDataSource(),
+      cartProductRemoteDataSource: _getCartProductRemoteDataSource());
+
+  CartProductRepository getCartProductRepository() => CartProductRepositoryImpl(
+        cartProductRemoteDataSource: _getCartProductRemoteDataSource(),
+        cartRemoteDataSource: _getCartRemoteDataSource(),
+      );
 
   //* ============= product feature =============
   ProductRemoteDataSource _getProductRemoteDataSource() =>
